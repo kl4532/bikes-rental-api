899eeb7484dfdeace1ac3d44dbbeebbcffd8fdfe
kl4532 <kornellukasiak@gmail.com>
2021-04-30 20:36:04 +0200

    init spring app

---
+++ src/main/java/com/bikesrentalapi/BikesRentalApiApplication.java
@@ -0,0 +1,13 @@
+package com.bikesrentalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikesRentalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikesRentalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bikesrentalapi/BikesRentalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikesrentalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikesRentalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
