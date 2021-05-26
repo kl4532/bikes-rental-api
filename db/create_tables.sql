55fa7b5eb6d85819fc755adf5ec25e07da648e91
kl4532 <kornellukasiak@gmail.com>
2021-05-26 07:33:44 +0200

    basic tables creation

---
+++ db/create_tables.sql
@@ -0,0 +1,53 @@
+-- run script in postgres: psql -U postgres -d Bikes_rental -a -f create_tables.sql
+-- or in docker postgres container:  docker exec -i <db_name> mysql -u<user> -p<pswd> < create_tables.sql
+
+drop table if exists bikes;
+drop table if exists bikes_booked_dates;
+drop table if exists booked_dates;
+
+
+create table bikes
+(
+    id SERIAL PRIMARY KEY,
+    picture BYTEA,
+    name VARCHAR(25),
+    description VARCHAR(2000),
+    price FLOAT8,
+    type  VARCHAR(25),
+    size VARCHAR(25),
+    bookedDates JSON,
+    reviews TEXT[],
+    rating INT,
+    gear TEXT[],
+    status VARCHAR(25)
+);
+
+create table bikes_booked_dates
+(
+    bike_id        integer not null
+        constraint bikes_booked_dates_bike_id_fkey
+            references bikes,
+    booked_date_id integer not null
+        constraint bikes_booked_dates_booked_date_id_fkey
+            references booked_dates
+);
+
+alter table bikes_booked_dates
+    owner to postgres;
+
+create table booked_dates
+(
+    booked_date_id serial      not null
+        constraint bikes_booked_dates_pk
+            primary key,
+    date_start     varchar(55) not null,
+    date_end       varchar(55) not null
+);
+
+alter table booked_dates
+    owner to postgres;
+
+create unique index bikes_booked_dates_id_uindex
+    on booked_dates (booked_date_id);
+
+
