4ecd38eb209f9cd110852dc9bd3f4fc1022b7349
kl4532 <kornellukasiak@gmail.com>
2021-06-11 21:40:03 +0200

    dont fill tables for now

--- db/makeDB.sh
+++ db/makeDB.sh
@@ -1,4 +1,4 @@
 cd bikes-rental-api/db
 ls
 psql -U postgres -d Bikes_rental -a -f create_tables.sql
-psql -U postgres -d Bikes_rental -a -f fill_tables.sql
+#psql -U postgres -d Bikes_rental -a -f fill_tables.sql
