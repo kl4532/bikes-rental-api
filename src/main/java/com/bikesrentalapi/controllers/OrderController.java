56ad9d7de311fa79aaab3d7d3f1f411ff12ead38
kl4532 <kornellukasiak@gmail.com>
2021-06-21 06:30:39 +0200

    add get order

--- src/main/java/com/bikesrentalapi/controllers/OrderController.java
+++ src/main/java/com/bikesrentalapi/controllers/OrderController.java
@@ -1,10 +1,14 @@
 package com.bikesrentalapi.controllers;
 
 import com.bikesrentalapi.models.OrderRaw;
+import com.bikesrentalapi.models.entities.Bike;
+import com.bikesrentalapi.models.entities.Order;
 import com.bikesrentalapi.repositories.OrderRepository;
 import com.bikesrentalapi.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -14,6 +18,18 @@ public class OrderController {
     private OrderRepository orderRepository;
     @Autowired
     private OrderService orderService;
+
+    @CrossOrigin
+    @GetMapping
+    public List<Order> list() {
+        return orderRepository.findAll();
+    }
+
+    @CrossOrigin
+    @GetMapping("/{id}")
+    public Order get(@PathVariable("id") Long id) {
+        return orderRepository.getById(id);
+    }
 
     @CrossOrigin
     @PostMapping
